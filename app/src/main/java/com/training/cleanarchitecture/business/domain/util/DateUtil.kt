dd83d78f24f4bb9bf72e6131b95711b39c9623d6
angeladimovska <angela.dimovska@echo.com.mk>
2023-01-17 00:12:51 +0100

    add missing files.

---
+++ app/src/main/java/com/training/cleanarchitecture/business/domain/util/DateUtil.kt
@@ -0,0 +1,29 @@
+package com.training.cleanarchitecture.business.domain.util
+
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DateUtil @Inject constructor(
+    private val dateFormat: SimpleDateFormat
+) {
+
+    fun removeTimeFromDateString(sd: String): String {
+        return sd.substring(0, sd.indexOf(" "))
+    }
+
+    fun convertFirebaseTimestampToDateString(timestamp: Timestamp): String {
+        return dateFormat.format(timestamp.toDate())
+    }
+
+    fun convertDateStringToFirebaseTimestamp(date: String): Timestamp {
+        return Timestamp(dateFormat.parse(date))
+    }
+
+    fun getCurrentTimestamp(): String {
+        return dateFormat.format(Date())
+    }
+}
